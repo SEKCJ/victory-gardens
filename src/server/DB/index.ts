80003e926282d1c153d022f1b3e48e8e7f019b74
SEKCJ <caesar@ea-vr.org>
2020-04-06 13:10:53 -0500

    fixed DB/index.ts

--- src/server/DB/index.ts
+++ src/server/DB/index.ts
@@ -3,14 +3,11 @@ import config from "../config";
 
 const pool = mysql.createPool(config.mysql);
 
-export const Query = <T = any>(query: string, values?: any) => {
-  // or: (query:string, values?: Array<string | number>)
+export const Query = (query: string, values?: Array<string | number>) => {
 
   const sql = mysql.format(query, values);
-  console.log(sql);
 
-  return new Promise<T>((resolve, reject) => {
-    // pool.query(query, values, (err, results) -- this is poor practice bc it allows the user to have access to the variables query and values in plain text. Updated version below.
+  return new Promise<Array<any>>((resolve, reject) => {
     pool.query(sql, (err, results) => {
       if (err) {
         reject(err);
