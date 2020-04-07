2e145701099c4977ffd73185b12a68c043a47093
SEKCJ <caesar@ea-vr.org>
2020-04-06 22:31:06 -0500

    added back to query function the type <T> and added it to the promise, on users.ts created an interface to define the user properties

--- src/server/DB/index.ts
+++ src/server/DB/index.ts
@@ -6,11 +6,11 @@ import Users from './users';
 
 const pool = mysql.createPool(config.mysql);
 
-export const Query = (query: string, values?: Array<string | number>) => {
+export const Query = <T = any> (query: string, values?: Array<string | number>) => {
 
   const sql = mysql.format(query, values);
 
-  return new Promise<Array<any>>((resolve, reject) => {
+  return new Promise<T>((resolve, reject) => {
     pool.query(sql, (err, results) => {
       if (err) {
         reject(err);
@@ -22,6 +22,6 @@ export const Query = (query: string, values?: Array<string | number>) => {
 };
 
 export default {
-  Tokens, 
+  Tokens,
   Users,
 };
--- src/server/DB/users.ts
+++ src/server/DB/users.ts
@@ -1,17 +1,26 @@
 import { Query } from './index';
 
 const findOneByEmail = async (email: string) => {
-    return Query('SELECT * FROM users WHERE email = ? LIMIT 1; --', [email]);
+    return Query<IUsers[]>('SELECT * FROM users WHERE email = ? LIMIT 1; --', [email]);
 }
 
 const findOneById = async (id: number) => {
-    return Query('SELECT * FROM users WHERE id = ?; --', [id])
+    return Query<IUsers[]>('SELECT * FROM users WHERE id = ?; --', [id])
 }
 
 const post = async (email: string, firstname: string, lastname: string, password: string) => {
     let values = [email, firstname, lastname, password];
-    return Query('INSERT INTO users(email, firstname, lastname, password) VALUES(?,?,?,?); --', values)
+    return Query<IUsers>('INSERT INTO users(email, firstname, lastname, password) VALUES(?,?,?,?); --', values)
 }
+
+export interface IUsers {
+    id: number,
+    email: string,
+    firstname: string,
+    lastname: string,
+    password: string
+}
+
 
 export default {
     findOneByEmail,
