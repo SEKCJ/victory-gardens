aa352d9b4b8491b9da1e2dd02d8391c5d555dece
SEKCJ <caesar@ea-vr.org>
2020-04-06 21:56:35 -0500

    changed server.ts file to include morgan dev logger and passport module for authentication

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,20 +1,24 @@
 import * as express from 'express';
 import * as path from 'path';
+import * as passport from 'passport'
+import * as morgan from 'morgan';
 import mainRouter from './Routes';
-
 
 const app = express();
 
-// Requires express.json to parse body requests
-app.use(express.json());
+app.use(passport.initialize());
 
 app.use(express.static('public'));
+
+app.use(express.json());
+
+app.use(morgan('dev'));
+
 app.use(mainRouter);
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'))
 })
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
