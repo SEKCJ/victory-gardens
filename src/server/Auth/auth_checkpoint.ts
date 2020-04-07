6cd24ea7f405565106aafa3298555e5e9d7ed968
SEKCJ <caesar@ea-vr.org>
2020-04-06 23:33:18 -0500

    added functions to check if user is a guest or if the user is an admin

---
+++ src/server/Auth/auth_checkpoint.ts
@@ -0,0 +1,24 @@
+import { RequestHandler, Request } from 'express';
+
+export const isGuest: RequestHandler = (req: ReqUser, res, next) => {
+    if (req.user && req.user.role === "guest") {
+        return next();
+    } else {
+        return res.sendStatus(401);
+    }
+}
+
+export const isAdmin: RequestHandler = (req: ReqUser, res, next) => {
+    if (req.user && req.user.role === "admin") {
+        return next();
+    } else {
+        return res.sendStatus(401);
+    }
+}
+
+interface ReqUser extends Request {
+    user: {
+        id: number,
+        role: string,
+    };
+}
