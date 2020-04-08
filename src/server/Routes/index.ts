5883c5c9c765617404c8a684bcf0ad06576834c2
SEKCJ <caesar@ea-vr.org>
2020-04-07 23:01:49 -0500

    imported Auth/index.ts default exports as authRouter on Routes/index.ts

--- src/server/Routes/index.ts
+++ src/server/Routes/index.ts
@@ -1,8 +1,11 @@
 import * as express from 'express';
+
 import apiRouter from '../API';
+import authRouter from '../Auth';
 
 const router = express.Router();
 
 router.use('/api', apiRouter);
+router.use('/auth', authRouter);
 
 export default router;
