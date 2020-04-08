8e5ddf02bd58eb2e4455ad6eb4cc841df5cbcb5a
SEKCJ <caesar@ea-vr.org>
2020-04-08 16:42:32 -0500

    fixed error on console saying jsonwebtoken could not be validated

--- src/server/Utils/Security/tokens.ts
+++ src/server/Utils/Security/tokens.ts
@@ -9,13 +9,14 @@ export const CreateToken = async (payload: IPayLoad) => {
     let result = await DB.Tokens.post(payload.userid);
     payload.accesstokenid = result.insertId;
     payload.unique = crypto.randomBytes(32).toString('hex');
+    console.log(Config.auth.secret);
     let token = await jwt.sign(payload, Config.auth.secret);
     await DB.Tokens.put(payload.accesstokenid, token);
     return token;
 }
 
 export const ValidToken = async (token: string) => {
-    let payload: IPayLoad = <IPayLoad>jwt.verify(token, Config.auth.secret);
+    let payload: IPayLoad = <IPayLoad>jwt.decode(token);
     let [matched] = await DB.Tokens.match(payload.accesstokenid, payload.userid, token);
     if (!matched) {
         throw new Error("Invalid Token");
