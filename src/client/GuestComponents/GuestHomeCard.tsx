154ba16e5b1c0bf7feaa5b3f0621b3afad7deffa
SEKCJ <caesar@ea-vr.org>
2020-04-12 13:09:19 -0500

    fixed GuestHomeCard.tsx

--- src/client/GuestComponents/GuestHomeCard.tsx
+++ src/client/GuestComponents/GuestHomeCard.tsx
@@ -1,11 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import { IAppProps } from '../GuestApp';
 import { RouteComponentProps } from 'react-router';
-import { Jumbotron, Form, Button, Spinner, Container } from 'react-bootstrap';
-import { api, setToken, Token } from '../Services/apiServices';
+import { IAppProps } from '../GuestApp';
+import { Jumbotron, Form, Button, Container, Spinner } from 'react-bootstrap';
+import { api, setToken, Token } from '../services/apiServices';
 
 const GuestHome: React.FC<ILoginProps> = props => {
-
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>('');
   const [error, setError] = useState<boolean>(false);
@@ -19,7 +18,6 @@ const GuestHome: React.FC<ILoginProps> = props => {
         }
       })
       .catch(error => {
-        console.log(error);
         setLoading(false)
       })
   }, [])
@@ -39,38 +37,40 @@ const GuestHome: React.FC<ILoginProps> = props => {
   if (loading === true) {
     return (
       <Container className="d-flex">
-        <Spinner animation="border" variant="warning"
-          className="mx-auto" style={{ "width": "30em", "height": "30em" }} />
+        <Spinner className="mx-auto my-3" animation="border" variant="warning" style={{ "width": "30em", "height": "30em" }} />
       </Container>
     )
   } else {
     return (
-      <Container>
+      <>
         <Jumbotron fluid>
           <h1>Victory Gardens</h1>
           <h4>How can YOU flatten the curve?</h4>
         </Jumbotron>
 
-        <Form>
-          <Form.Group controlId="formBasicEmail">
-            <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email"
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)} />
-            <Form.Text className="text-muted">
-            </Form.Text>
-          </Form.Group>
+        <Container>
+          <Form>
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email"
+                value={email} onChange={(e: any) => setEmail(e.target.value)} />
+              <Form.Text className="text-muted">
+              </Form.Text>
+            </Form.Group>
 
-          <Form.Group controlId="formBasicPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password"
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)} />
-          </Form.Group>
-          <Button variant="primary" type="submit" onClick={handleLogin}>Submit</Button>
-          <div>
-            <Button href="/guestsignup" variant="link" type="submit">Don't have an account? Click here to create one!</Button>
-          </div>
-        </Form>
-      </Container>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password"
+                value={password} onChange={(e: any) => setPassword(e.target.value)} />
+            </Form.Group>
+            <Button variant="primary" type="submit"
+              onClick={handleLogin}>Submit</Button>
+            <div>
+              <Button href="/guestsignup" variant="link" type="submit">Don't have an account? Click here to create one!</Button>
+            </div>
+          </Form>
+        </Container>
+      </>
     )
   }
 }
