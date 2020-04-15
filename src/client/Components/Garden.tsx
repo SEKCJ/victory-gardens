34d4c87ada3161ae2a131621ea020b08be890357
Cheyenne Mangum <cheyennemangum@gmail.com>
2020-04-14 23:51:53 -0500

    new rabbit hole changes to Garden visualizer with help of cabbage patch kid (CM)

--- src/client/Components/Garden.tsx
+++ src/client/Components/Garden.tsx
@@ -1,84 +1,91 @@
-import React, { useState, useEffect } from 'react';
-import { RouteComponentProps } from 'react-router-dom';
-import { api } from '../Services/apiServices';
-import { Card, ListGroup, ListGroupItem, Container, Row, Col, Button, Image } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import { RouteComponentProps } from "react-router-dom";
+import { api } from "../Services/apiServices";
+import {
+  Card,
+  ListGroup,
+  ListGroupItem,
+  Container,
+  Row,
+  Col,
+  Button,
+  Image,
+} from "react-bootstrap";
 // import styles from '../scss/app.scss';
 // import '../styles.css';
 import { IVegetables } from "../../server/Models/index";
 
-interface IGardenProps extends RouteComponentProps<{ id: string }> { }
+interface IGardenProps extends RouteComponentProps<{ id: string }> {}
 const Garden: React.FC<IGardenProps> = ({
-    match: {
-        params: { id },
-    },
+  match: {
+    params: { id },
+  },
 }) => {
-    const [Garden, setGarden] = useState<any>();
+  const [userVeggies, setUserVeggies] = useState([
+    {
+      id: 1,
+      name: "Cabbage",
+      url:
+        "https://harvesttotable.com/wp-content/uploads/2009/01/Cabbage-bigstock-Salad-Species-That-Includes-Se-251274103-scaled.jpg",
+      row: 2,
+      column: 5,
+    },
+  ]);
+  const [soilArray, setSoilArray] = useState<any>(
+    new Array(20).fill(0).map(() => new Array(10).fill(0))
+  );
 
-    let fetchAPI = async () => {
-        let [response] = await api(`/api/vegetables/${id}`);
-        makeGarden(response);
-    };
-    let makeGarden = (resObj: any) => {
-        let vgId = resObj.id;
-        let vgName = resObj.name;
-        let vgPosition = resObj.position;
-        let vgSpacing = resObj.spacing;
-        let vgSandP = resObj.sow_and_plant;
-        let vgImg = resObj.url;
+  useEffect(() => {
+    const placedVeggiesArray = [...soilArray];
 
-        setGarden(
-            <section id="view">
-                <Container fluid="md">
-                    <div id="board">
-                        <div id="plants"></div>
-                        <div id="soil">
-                            {/* need a top row for things to start insde (CM) */}
-                            <Row id='1'>
-                            <Col className="plot" sm='1'><Image src={'/assets/dirt.jpg'} style={{ 'width': '100%' }}></Image></Col>
-                                <Col className="plot" sm='1'><Image src={'/assets/dirt.jpg'} style={{ 'width': '100%' }}></Image></Col>
-                                <Col className="plot" sm='1'><Image src={'/assets/dirt.jpg'} style={{ 'width': '100%' }}></Image></Col>
-                                <Col className="plot" sm='1'><Image src={'/assets/dirt.jpg'} style={{ 'width': '100%' }}></Image></Col>
-                                <Col className="plot" sm='1'><Image src={'/assets/dirt.jpg'} style={{ 'width': '100%' }}></Image></Col>
-                                <Col className="plot" sm='1'><Image src={'/assets/dirt.jpg'} style={{ 'width': '100%' }}></Image></Col>
-                                <Col className="plot" sm='1'><Image src={'/assets/dirt.jpg'} style={{ 'width': '100%' }}></Image></Col>
-                                <Col className="plot" sm='1'><Image src={'/assets/dirt.jpg'} style={{ 'width': '100%' }}></Image></Col>
-                                <Col className="plot" sm='1'><Image src={'/assets/dirt.jpg'} style={{ 'width': '100%' }}></Image></Col>
-                                <Col className="plot" sm='1'><Image src={'/assets/dirt.jpg'} style={{ 'width': '100%' }}></Image></Col>
-                            </Row>
-                            <Row id='2'>
-                                <Col className="plot" sm='1'><Image src={'/assets/dirt.jpg'} style={{ 'width': '100%' }}></Image></Col>
-                                <Col className="plot" sm='1'><Image src={'/assets/dirt.jpg'} style={{ 'width': '100%' }}></Image></Col>
-                                <Col className="plot" sm='1'><Image src={'/assets/dirt.jpg'} style={{ 'width': '100%' }}></Image></Col>
-                                <Col className="plot" sm='1'><Image src={'/assets/dirt.jpg'} style={{ 'width': '100%' }}></Image></Col>
-                                <Col className="plot" sm='1'><Image src={'/assets/dirt.jpg'} style={{ 'width': '100%' }}></Image></Col>
-                                <Col className="plot" sm='1'><Image src={'/assets/dirt.jpg'} style={{ 'width': '100%' }}></Image></Col>
-                                <Col className="plot" sm='1'><Image src={'/assets/dirt.jpg'} style={{ 'width': '100%' }}></Image></Col>
-                                <Col className="plot" sm='1'><Image src={'/assets/dirt.jpg'} style={{ 'width': '100%' }}></Image></Col>
-                                <Col className="plot" sm='1'><Image src={'/assets/dirt.jpg'} style={{ 'width': '100%' }}></Image></Col>
-                                <Col className="plot" sm='1'><Image src={'/assets/dirt.jpg'} style={{ 'width': '100%' }}></Image></Col>
-                            </Row>
-                            <Row id='3'>
-                                <Col className="plot" sm='1'><Image src={'/assets/dirt.jpg'} style={{ 'width': '100%' }}></Image></Col>
-                                <Col className="plot" sm='1'><Image src={'/assets/dirt.jpg'} style={{ 'width': '100%' }}></Image></Col>
-                                <Col className="plot" sm='1'><Image src={'/assets/dirt.jpg'} style={{ 'width': '100%' }}></Image></Col>
-                                <Col className="plot" sm='1'><Image src={'/assets/dirt.jpg'} style={{ 'width': '100%' }}></Image></Col>
-                                <Col className="plot" sm='1'><Image src={'/assets/dirt.jpg'} style={{ 'width': '100%' }}></Image></Col>
-                                <Col className="plot" sm='1'><Image src={'/assets/dirt.jpg'} style={{ 'width': '100%' }}></Image></Col>
-                                <Col className="plot" sm='1'><Image src={'/assets/dirt.jpg'} style={{ 'width': '100%' }}></Image></Col>
-                                <Col className="plot" sm='1'><Image src={'/assets/dirt.jpg'} style={{ 'width': '100%' }}></Image></Col>
-                                <Col className="plot" sm='1'><Image src={'/assets/dirt.jpg'} style={{ 'width': '100%' }}></Image></Col>
-                                <Col className="plot" sm='1'><Image src={'/assets/dirt.jpg'} style={{ 'width': '100%' }}></Image></Col>
-                            </Row>
-                        </div>
-                    </div>
-                </Container>
-            </section>
-        )
-    };
-    useEffect(() => {
-        fetchAPI();
-    }, []);
+    for (let i = 0; i < userVeggies.length; i++) {
+      const { row, column } = userVeggies[i];
+      placedVeggiesArray[row].splice(column, 1, userVeggies[i]);
+    }
+    setSoilArray(placedVeggiesArray);
+  }, [userVeggies]);
 
-    return <React.Fragment>{Garden}</React.Fragment>;
-}
+  let fetchAPI = async () => {
+    let [response] = await api(`/api/vegetables/${id}`);
+    makeGarden(response);
+  };
+  let makeGarden = (resObj: any) => {
+    let vgId = resObj.id;
+    let vgName = resObj.name;
+    let vgPosition = resObj.position;
+    let vgSpacing = resObj.spacing;
+    let vgSandP = resObj.sow_and_plant;
+    let vgImg = resObj.url;
+  };
+  useEffect(() => {
+    fetchAPI();
+  }, []);
+
+  return (
+    <section id="view">
+      <Container fluid="md">
+        <div id="board">
+          <div id="soil">
+            {/* need a top row for things to start insde (CM) */}
+            {soilArray.map((row, rowIndex) => (
+              <div className="dirtRow" key={rowIndex}>
+                {row.map((column, columnIndex) => (
+                  <div key={columnIndex} className="plot">
+                    <Image
+                      src={column.url ? column.url : "/assets/dirt.jpg"}
+                      style={{ width: "100%" }}
+                      onClick={() =>
+                        console.log({ row: rowIndex, column: columnIndex })
+                      }
+                    ></Image>
+                  </div>
+                ))}
+              </div>
+            ))}
+          </div>
+        </div>
+      </Container>
+    </section>
+  );
+};
 export default Garden;
