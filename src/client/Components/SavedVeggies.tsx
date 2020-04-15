bd3cab172550abc892c6fcf0184b79e3d924222c
SEKCJ <caesar@ea-vr.org>
2020-04-15 15:10:55 -0500

    fixed deletion on SavedVeggies.tsx

--- src/client/Components/SavedVeggies.tsx
+++ src/client/Components/SavedVeggies.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Card, Container, Jumbotron, Row, Col, Button, Collapse } from 'react-bootstrap';
+import { Card, Container, Jumbotron, Row, Col, Button, Collapse, Alert } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { IAppProps } from '../App';
 import { api, Token } from '../Services/apiServices';
@@ -7,57 +7,87 @@ import { api, Token } from '../Services/apiServices';
 const SavedVeggies: React.FC<IAppProps> = props => {
 
     const [apiArray, setApiArray] = useState([]);
+    const [deleted, setDeleted] = useState<boolean>(false);
+    const [deleting, setDeleting] = useState<JSX.Element>()
 
     let fetchAPI = async () => {
         let response = await api(`/api/savedvegetables/${Token}`);
         makeCards(response)
+        setDeleted(false)
+        setDeleting(<div></div>)
     }
 
     let handleClick = async (e: React.MouseEvent<HTMLButtonElement>, vegId: number) => {
-        let response = await api(`/api/savedvegetables/${vegId}`, "DELETE", {Token})
-        console.log(response)
+        setDeleting(
+            <Container className="d-flex mt-4">
+                <Alert variant="warning" className="mx-auto col-sm-6 d-flex flex-column">
+                    Deleting Vegetable...
+                </Alert>
+            </Container>
+        )
+        let response = await api(`/api/savedvegetables/${vegId}`, "DELETE", { Token })
+        setDeleted(true)
     }
 
     let makeCards = (resObj: any) => {
-        let cardMemory = resObj.map((element: any, index: any) => {
-            let veggieImg = element.url;
-            let veggieName = element.name;
-            let veggieId = element.id;
-            let veggieSciName = element.sci_name
+        if (resObj.length === 0) {
+            setApiArray([(
+                <Row key="0">
+                    <Alert variant="warning" className="mx-auto col-sm-8 d-flex flex-column">
+                        <h6 className="mx-auto">Looks like no vegetables have been added yet...</h6>
+                        <p className="mx-auto mb-1">Click For{" "}
+                        <Alert.Link as={Link} to="/choose">Help</Alert.Link>
+                        {" "}on How To Choose!</p>
+                        <p className="mx-auto mb-0">
+                            Click To{" "}
+                            <Alert.Link className="mx-auto" as={Link} to="/veggies">View</Alert.Link>
+                            {" "}All Vegetables
+                        </p>
 
-            return (
-                <Row key={veggieId} className="d-flex">
-                    <Card className="mx-auto col-sm-8 px-0">
-                        <div className="d-flex flex-row">
-                            <Card.Img variant="top" style={{ "width": "10em" }}
-                                src={veggieImg} />
-                            <Card.ImgOverlay className="px-0 py-0" style={{ "width": "5em" }}>
-                                <Button variant="danger" className="px-1 py-1"
-                                    onClick={(e: React.MouseEvent<HTMLButtonElement>) => { handleClick(e, veggieId) }}>
-                                    <small style={{ "fontSize": "0.75em" }}>X</small>
-                                </Button>
-                            </Card.ImgOverlay>
-                            <Card.Body>
-                                <Card.Title>{veggieName}</Card.Title>
-                                <Card.Text>
-                                    {veggieSciName}
-                                </Card.Text>
-                            </Card.Body>
-
-                            <Button variant="primary" as={Link} to={`/veggies/${veggieId}`}>Read More</Button>
-                        </div>
-                    </Card>
+                    </Alert>
                 </Row>
+            )])
+        } else {
+            let cardMemory = resObj.map((element: any, index: any) => {
+                let veggieImg = element.url;
+                let veggieName = element.name;
+                let veggieId = element.id;
+                let veggieSciName = element.sci_name
 
-            )
-        })
+                return (
+                    <Row key={veggieId} className="d-flex">
+                        <Card className="mx-auto col-sm-8 px-0">
+                            <div className="d-flex flex-row">
+                                <Card.Img variant="top" style={{ "width": "10em" }}
+                                    src={veggieImg} />
+                                <Card.ImgOverlay className="px-0 py-0" style={{ "width": "5em" }}>
+                                    <Button variant="danger" className="px-1 py-1"
+                                        onClick={(e: React.MouseEvent<HTMLButtonElement>) => { handleClick(e, veggieId) }}>
+                                        <small style={{ "fontSize": "0.75em" }}>X</small>
+                                    </Button>
+                                </Card.ImgOverlay>
+                                <Card.Body>
+                                    <Card.Title>{veggieName}</Card.Title>
+                                    <Card.Text>
+                                        {veggieSciName}
+                                    </Card.Text>
+                                </Card.Body>
 
-        setApiArray(cardMemory)
+                                <Button variant="primary" as={Link} to={`/veggies/${veggieId}`}>Read More</Button>
+                            </div>
+                        </Card>
+                    </Row>
+
+                )
+            })
+
+            setApiArray(cardMemory)
+        }
     }
     // useEffect [] same as componentDidMount()
     useEffect(() => {
         fetchAPI()
-    }, [])
+    }, [deleted])
 
 
     return (
@@ -68,6 +98,7 @@ const SavedVeggies: React.FC<IAppProps> = props => {
                     <h1>My Garden</h1>
                 </Container>
             </Jumbotron>
+            {deleting}
             {apiArray}
 
         </Container>
