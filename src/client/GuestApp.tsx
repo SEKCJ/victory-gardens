d0a883e63a7242411a206290cf392b0da8ee91b2
student <student@Admins-iMac.local>
2020-04-15 17:59:00 -0500

    added about for guest and user

--- src/client/GuestApp.tsx
+++ src/client/GuestApp.tsx
@@ -3,11 +3,12 @@ import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import GuestNavigate from './GuestComponents/GuestNavbar';
 import GuestHomeCard from './GuestComponents/GuestHomeCard';
 import GuestVeggies from './GuestComponents/GuestVeggies';
-import GuestChoose from './GuestComponents/GuestGhoose';
+// import GuestChoose from './GuestComponents/GuestGhoose';
 import GuestSignUp from './GuestComponents/GuestSignUp';
 //import GuestLogin from './GuestComponents/GuestLogin';
 import GuestAbout from './GuestComponents/GuestAbout';
-import GSingleVeg from './GuestComponents/GSingleVeg';
+// import GSingleVeg from './GuestComponents/GuestSingleVeg';
+import HelpChoose from './Components/UserChoose';
 
 const GuestApp: React.FC<IAppProps> = props => {
 
@@ -16,13 +17,12 @@ const GuestApp: React.FC<IAppProps> = props => {
             <GuestNavigate />
             <Switch>
                 <Route exact path="/guestveggies" component={GuestVeggies} />
-                <Route exact path="/guestchoose" component={GuestChoose} />
+               <Route exact path="/guestchoose" component={HelpChoose} /> 
                 {/* <Route exact path= "/joingarden" component={UserSignUp}/> */}
                 <Route exact path="/guestsignup" component={GuestSignUp} />
                 <Route exact path="/guestabout" component={GuestAbout} />
-                <Route exact path="/guestsingleveg" component={GSingleVeg} />
+                {/* <Route exact path="/guestsingleveg/:id" component={GSingleVeg} />  */}
                 <Route path="/" component={GuestHomeCard} />
-
             </Switch>
         </Router >
     )
