d36414a532ead6c81f7d4bc0c2d46010fe7e89f8
IB_Student <student@InnovateBhams-MacBook-Pro.local>
2020-04-16 13:49:11 -0500

    changed user homepage RR

--- src/client/components/UserHome.tsx
+++ src/client/components/UserHome.tsx
@@ -1,16 +1,43 @@
 import React, { } from 'react';
 import { IAppProps } from '../App';
-import { Jumbotron, Button, Container, Row, Col, Image } from 'react-bootstrap';
+import { Jumbotron, Button, Container, Row, Col, Image, Carousel } from 'react-bootstrap';
 
 
-const UserHome: React.FC<IAppProps> = props => {
-  return (
-      <Jumbotron fluid>
-        <h1>Victory Gardens</h1>
-        <h4>How can YOU flatten the curve?</h4>
-    
-     
+const UserHome: React.FC<IAppProps> = props => { 
+  return(
+    <Container className="justify-content-center">
+    <Jumbotron fluid className="rounded">
+    <main className="container my-5">
+      <h1 className="text-primary">Welcome to Victory Gardens</h1><p className="text-primary">How will YOU flatten the curve?</p>
+      <div className="video">  <h3 className="text-primary"></h3>
+        {/* <iframe src="https://archive.org/embed/Kitchencaravan-TheVictoryGardenGrowsAgain209-2" width="640" height="480" frameBorder="0" allowFullScreen></iframe> */}
+        {/* // webkitallowfullscreen="true"
+        // mozallowfullscreen="true"  */}
+    </div> 
+    {/* <p className="text-muted">Don't have an account yet? Click<Button href="/guestsignup" variant="link" type="submit">here</Button>to join Victory Gardens!</p>  */}
+    </main>
+  </Jumbotron>
+  <div className="justify-content">  
+  <Carousel className="rounded">
+  <Carousel.Item>
+   <Image className="d-block w-100"
+          src="assets/victorydefintion2.png"
+          alt="First slide" rounded />
+  
+   </Carousel.Item>
+      <Carousel.Item>
+    <Image className="d-block w-100"
+          src="assets/victoryabout3.jpg"
+          alt="Third slide" rounded />
+      </Carousel.Item>
+    </Carousel>
+    </div>
+  </Container>
+  
+   )
+  }
 
+{/* 
       <Container>
   <Row>
     <Col xs={6} md={4}>
@@ -36,9 +63,9 @@ const UserHome: React.FC<IAppProps> = props => {
 </Container> 
 <br></br>
  <Button variant="success" href="/aboutus">About Us</Button>
- </Jumbotron>
+
 
   )
-}
+} */}
 
 export default UserHome;
