5e9606660c59f7b6aadf7ad0f3d29abdb51e6c06
SEKCJ <caesar@ea-vr.org>
2020-04-17 00:44:53 -0500

    fixed Navbar and added functions to successfully log in and make a spinner to avoid multiple post requests, added margin to bottom of carousels, took out login feature from GuestHomeCard.tsx

--- src/client/GuestComponents/GuestHomeCard.tsx
+++ src/client/GuestComponents/GuestHomeCard.tsx
@@ -1,14 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { RouteComponentProps } from 'react-router';
 import { IAppProps } from '../GuestApp';
-import { Jumbotron, Form, Button, Container, Spinner, Alert, Carousel, Image, Card} from 'react-bootstrap';
+import { Jumbotron,  Container, Spinner, Alert, Carousel, Image, Row, Col } from 'react-bootstrap';
 import { api, setToken, Token } from '../services/apiServices';
 
 const GuestHome: React.FC<ILoginProps> = props => {
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>('');
-  const [error, setError] = useState<boolean>(false);
-  const [invalid, setInvalid] = useState<JSX.Element>();
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
@@ -23,30 +19,6 @@ const GuestHome: React.FC<ILoginProps> = props => {
       })
   }, [])
 
-  const handleLogin = async (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
-    let result = await api<{ token: string }>('/auth/login', "POST", { email, password });
-    if (result?.token) {
-      setToken(result.token);
-      props.history.push("/")
-      window.location.reload()
-    } else {
-      setError(true)
-    }
-  }
-
-  useEffect(() => {
-    if (error === true) {
-      setInvalid(
-        <Alert variant="danger" className="col-sm-8 mx-auto" onClose={() => setError(false)} dismissible>
-          <Alert.Heading>Invalid Login</Alert.Heading>
-          <p>Check your credentials and try again.</p>
-        </Alert>
-      )
-    } else {
-      setInvalid(<div></div>)
-    }
-  }, [error])
 
   if (loading === true) {
     return (
@@ -55,69 +27,66 @@ const GuestHome: React.FC<ILoginProps> = props => {
       </Container>
     )
   } else {
-    return ( 
+    return (
       <Container className="d-flex flex-column">
         <Jumbotron fluid className="rounded">
           <h1>Victory Gardens</h1>
           <h4>How can YOU flatten the curve?</h4>
           <br></br>
-          <p>Login</p>
-          <div>
-          <Form className="flex-center">
-            <Form.Group controlId="formBasicEmail">
-              {/* <Form.Label>Email address</Form.Label> */}
-              <Form.Control type="email" placeholder="Enter email"
-                value={email} onChange={(e: any) => setEmail(e.target.value)} />
-              <Form.Text className="text-muted">
-              </Form.Text>
-            </Form.Group>
+          <Container>
+            <Row>
+              <Col xs={6} md={4}>
+                <Image
+                  width={300}
+                  height={200}
+                  src="https://c0.wallpaperflare.com/preview/716/333/216/tomato-tomates-garden-vegetable.jpg" thumbnail />
+              </Col>
+              <Col xs={6} md={4}>
+                <Image
+                  width={300}
+                  height={200}
+                  src="https://c1.peakpx.com/wallpaper/454/298/418/vegetable-garden-varieties-of-kale-wallpaper-preview.jpg" thumbnail />
+              </Col>
 
-            <Form.Group controlId="formBasicPassword">
-              {/* <Form.Label>Password</Form.Label> */}
-              <Form.Control type="password" placeholder="Password"
-                value={password} onChange={(e: any) => setPassword(e.target.value)} />
-            </Form.Group>
-            <Button variant="primary" type="submit"
-              onClick={handleLogin}>Login</Button>
-            <div>
-              <Button href="/guestsignup" variant="link" type="submit">Don't have an account? Click here to create one!</Button>
-            </div>
-          </Form>
-          </div>
-          <div className="d-flex">
-            {invalid}
-          </div>
+              <Col xs={6} md={4}>
+                <Image
+                  width={300}
+                  height={200}
+                  src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Corn_On_The_Cobb_From_The_Garden_%28120856897%29.jpeg/1600px-Corn_On_The_Cobb_From_The_Garden_%28120856897%29.jpeg" thumbnail />
+              </Col>
+            </Row>
+          </Container>
         </Jumbotron>
 
-     <Jumbotron fluid className="rounded">
-        <main className="container my-5">
-          <h1 className="text-primary">About Victory Gardens</h1>
-          <div className="video">  <h3 className="text-primary"></h3>
-            {/* <iframe src="https://archive.org/embed/Kitchencaravan-TheVictoryGardenGrowsAgain209-2" width="640" height="480" frameBorder="0" allowFullScreen></iframe> */}
-            {/* // webkitallowfullscreen="true"
+        <Jumbotron fluid className="rounded">
+          <main className="container ">
+            <h1 className="text-primary">About Victory Gardens</h1>
+            <div className="video">  <h3 className="text-primary"></h3>
+              {/* <iframe src="https://archive.org/embed/Kitchencaravan-TheVictoryGardenGrowsAgain209-2" width="640" height="480" frameBorder="0" allowFullScreen></iframe> */}
+              {/* // webkitallowfullscreen="true"
             // mozallowfullscreen="true"  */}
-        </div> 
-        {/* <p className="text-muted">Don't have an account yet? Click<Button href="/guestsignup" variant="link" type="submit">here</Button>to join Victory Gardens!</p>  */}
-        </main>
- </Jumbotron>
-    <div className="justify-content">  
-    <Carousel className="rounded">
-     <Carousel.Item>
-       <Image className="d-block w-100"
-              src="assets/victorydefintion2.png"
-              alt="First slide" rounded />
+            </div>
+            {/* <p className="text-muted">Don't have an account yet? Click<Button href="/guestsignup" variant="link" type="submit">here</Button>to join Victory Gardens!</p>  */}
+          </main>
+        </Jumbotron>
+        <div className="justify-content">
+          <Carousel className="rounded mb-5">
+            <Carousel.Item>
+              <Image className="d-block w-100"
+                src="assets/victorydefintion2.png"
+                alt="First slide" rounded />
 
-       </Carousel.Item>
-          <Carousel.Item>
-        <Image className="d-block w-100"
-              src="assets/victoryabout3.jpg"
-              alt="Third slide" rounded />
-          </Carousel.Item>
-        </Carousel>
+            </Carousel.Item>
+            <Carousel.Item>
+              <Image className="d-block w-100"
+                src="assets/victoryabout3.jpg"
+                alt="Third slide" rounded />
+            </Carousel.Item>
+          </Carousel>
         </div>
-    </Container>
+      </Container>
 
-      
+
     )
   }
 }
--- src/client/GuestComponents/GuestNavbar.tsx
+++ src/client/GuestComponents/GuestNavbar.tsx
@@ -1,63 +1,118 @@
-import React, { } from 'react';
-import { Nav, Navbar, NavDropdown, Form, Button} from 'react-bootstrap';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { IAppProps } from '../GuestApp';
+import { api, Token, setToken } from '../Services/apiServices';
+import { Nav, Navbar, NavDropdown, Form, Button, DropdownButton, Alert, Container, Spinner } from 'react-bootstrap';
 
 
-const GuestNavigate: React.FC<IAppProps> = props => {
+const GuestNavigate: React.FC<any> = props => {
 
-return (
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const [error, setError] = useState<boolean>(false);
+  const [invalid, setInvalid] = useState<JSX.Element>();
+  const [btnState, setBtnState] = useState<boolean>(false);
+  const [loading, setLoading] = useState<JSX.Element>();
+
+  const handleLogin = async () => {
+    setBtnState(true);
+    let result = await api<{ token: string }>('/auth/login', "POST", { email, password });
+    if (result?.token) {
+      setToken(result.token);
+      // props.history.push("/")
+      setBtnState(false);
+      window.location.reload()
+    } else {
+      setError(true)
+      setBtnState(false)
+    }
+  }
+
+  useEffect(() => {
+    if (error === true) {
+      setInvalid(
+        <Alert variant="danger" className="mx-2 my-1" onClose={() => setError(false)} dismissible>
+          <Alert.Heading>Invalid Login</Alert.Heading>
+          <p>Check your credentials and try again.</p>
+        </Alert>
+      )
+    } else {
+      setInvalid(<div></div>)
+    }
+  }, [error])
+
+  useEffect(() => {
+    if (btnState === true) {
+      setInvalid(<div></div>)
+      setLoading(
+        <Container className="d-flex">
+          <Spinner className="mx-auto" animation="border" variant="info"
+            style={{ "width": "5em", "height": "5em" }} />
+        </Container>
+      )
+    } else {
+      setLoading(
+        <Form className="px-2 py-2 mb-0 d-flex flex-column">
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email"
+              style={{ "width": "15em" }} value={email}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setEmail(e.target.value)} />
+            <Form.Text className="text-muted">
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" value={password}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)} />
+          </Form.Group>
+          <Button variant="primary" onClick={() => handleLogin()}
+            type="submit" className="mx-auto" as={Link} to="/">
+            Submit
+            </Button>
+        </Form>
+      )
+    }
+  }, [btnState, email, password])
+
+  return (
     <Navbar variant="dark" bg="dark" expand="lg" className="sticky-top">
-        <Navbar.Brand href="/"> <img src={window.location.origin + "/assets/VGLogo.png"}
-        style={{"width": "3em"}} /></Navbar.Brand>
-        <Navbar.Toggle aria-controls="navigate"/>
-        <Navbar.Collapse id="navigate">
-            <Nav className="mr-auto">
-               
-                <Navbar.Collapse id="navigate">  <Nav>
-                 <Nav.Link as={Link} to="/guestchoose">Help Me Choose</Nav.Link>
-                <NavDropdown className="mr-auto" title="Browse All" id="browse-dropdown">
-                    <NavDropdown.Item as={Link} to='/guestveggies'>Veggies</NavDropdown.Item>
-                    <NavDropdown.Item as={Link} to='/'>Herbs</NavDropdown.Item>
+      <Navbar.Brand href="/"> <img src={window.location.origin + "/assets/VGLogo.png"}
+        style={{ "width": "3em" }} /></Navbar.Brand>
+      <Navbar.Toggle aria-controls="navigate" />
+      <Navbar.Collapse id="navigate">
+        <Nav className="mr-auto">
 
-                </NavDropdown>
-                {/* <Nav.Link as={Link} to="/">Calendar</Nav.Link> */}
-                <Nav.Link as={Link} to="/guestabout">About Victory Gardens</Nav.Link>
-                </Nav>
-            </Navbar.Collapse>
+          <Navbar.Collapse id="navigate">  <Nav>
+            <Nav.Link as={Link} to="/guestchoose">Help Me Choose</Nav.Link>
+            <NavDropdown className="mr-auto" title="Browse All" id="browse-dropdown">
+              <NavDropdown.Item as={Link} to='/guestveggies'>Veggies</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to='/'>Herbs</NavDropdown.Item>
 
-
-            </Nav>
-            <Nav className="mr-5">
-            <NavDropdown className="mr-auto" title="Login" id="login-dropdown">
-                    <NavDropdown.Item><Form>
-<Form.Group controlId="formBasicEmail">
-  <Form.Label>Email address</Form.Label>
-  <Form.Control type="email" placeholder="Enter email" />
-  <Form.Text className="text-muted">
-  </Form.Text>
-</Form.Group>
-
-<Form.Group controlId="formBasicPassword">
-  <Form.Label>Password</Form.Label>
-  <Form.Control type="password" placeholder="Password" />
-</Form.Group>
-<Button variant="primary" type="submit">
-  Submit
-</Button>
-</Form>
-</NavDropdown.Item>
-                    
-
-                </NavDropdown>
-                <Nav.Link as={Link} to="/guestsignup">Signup</Nav.Link>
+            </NavDropdown>
+            {/* <Nav.Link as={Link} to="/">Calendar</Nav.Link> */}
+            <Nav.Link as={Link} to="/guestabout">About Victory Gardens</Nav.Link>
+          </Nav>
+          </Navbar.Collapse>
 
 
-            </Nav>
-        </Navbar.Collapse>
+        </Nav>
+        <Nav className="mr-5">
+
+          <DropdownButton alignRight title="Login" variant="success" id="login-button">
+            {loading}
+            {invalid}
+          </DropdownButton>
+
+          <Nav.Link as={Link} to="/guestsignup">Signup</Nav.Link>
+
+
+        </Nav>
+      </Navbar.Collapse>
 
     </Navbar >
-)
+  )
 }
 
 
